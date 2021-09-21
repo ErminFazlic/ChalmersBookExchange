31f677fd671ae6d44260ffe7580ab699d57dd5be
arian <arian.ka.1990@gmail.com>
2021-09-21 13:01:27 +0200

    Added post form.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,24 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ChalmersBookExchange.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ChalmersBookExchange.Models;
+using ChalmersBookExchange.Views.Home;
 
 namespace ChalmersBookExchange.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPostController _postController;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPostController postController)
         {
             _logger = logger;
+            _postController = postController;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Posts()
+        {
+            ViewBag.Title = "Posts";
+            var model = new PostsModel(_postController);
+            return View(model);
         }
 
         public IActionResult Favorites()
@@ -32,6 +42,25 @@ namespace ChalmersBookExchange.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+        [HttpPost]
+        public ActionResult CreatePostForm(string BookName, string CourseCode, string Description, int Price, string Location, bool Shippable, bool Meetup)
+        {
+            var email = _userManager.GetUserName(_httpContextAccessor.HttpContext.User);
+            var post = new Post
+            {
+                ID = _context.User.FirstOrDefault(x=> x.Email==email).ID,
+                BookName = BookName,
+                CourseCode = CourseCode,
+                Description = Description,
+                Price = Price,
+                Location = Location,
+                Shippable = Shippable,
+                Meetup = Meetup
+            };
+            var created = CreatePost(post);
+            return Microsoft.EntityFrameworkCore.Metadata.Internal.View("Index");
+
         }
     }
 }
--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -2,19 +2,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using ChalmersBookExchange.Data;
 using ChalmersBookExchange.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ChalmersBookExchange.Controllers
 {
     public class PostController : IPostController
     {
         private readonly MyDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public PostController(MyDbContext context)
+        public PostController(MyDbContext context, UserManager<IdentityUser> userManager, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
         }
+        
+        
+       
+        public bool CreatePost(Post post)
+        {
+            var exists =  _context.Post.FirstOrDefault(x => x.ID == post.ID);
 
+            if (exists is null)
+            {
+                _context.Post.Add(post);
+                var created = _context.SaveChanges();
+                return created > 0;
+            }
+
+            return false;
+        }
         public async Task<bool> CreatePostAsync(Post post)
         {
             var exists = await _context.Post.FirstOrDefaultAsync(x => x.ID == post.ID);
