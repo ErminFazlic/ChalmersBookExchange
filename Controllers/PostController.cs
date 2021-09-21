94af2e4b800c3a4b2833d51e192b7405f13c0d8a
ErminFazlic <Erminfazlic97@gmail.com>
2021-09-21 11:01:45 +0200

    Added constructor in PostController.cs

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,7 +1,14 @@
+using ChalmersBookExchange.Data;
+
 namespace ChalmersBookExchange.Controllers
 {
     public class PostController
     {
-        
+        private readonly MyDbContext _context;
+
+        public PostController(MyDbContext context)
+        {
+            _context = context;
+        }
     }
 }
