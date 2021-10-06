d2299d072e55f4c89fe7c23c04ad093f6c2d57b6
Svenub <77622367+Svenub@users.noreply.github.com>
2021-10-06 14:27:34 +0200

    Added methods for editing and deleting post,
    Co-authored-by: Negin <bakhtiari_negin@yahoo.com>
    Co-authored-by: Cynthia <kozma@student.chalmers.se>
    Co-authored-by: Petra <petra.nissan@hotmail.com>

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -17,6 +17,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore.Internal;
+using Npgsql.Replication;
+using Index = System.Index;
+
 
 namespace ChalmersBookExchange.Controllers
 {
@@ -82,7 +85,6 @@ namespace ChalmersBookExchange.Controllers
                 reversedPosts[i] = posts[j];
                 j++;
             }
-
             return reversedPosts;
         }
 
@@ -96,113 +98,14 @@ namespace ChalmersBookExchange.Controllers
 
         public Post[] GetMyPosts(Guid userid)
         {
-
             var posts = _context.Post.Where(x => x.Poster == userid).ToArray();
             posts = ReversePosts(posts);
             return posts;
-
         }
-
-        // POST: Movies/Delete/5
-        public Post[] DeleteConfirmed(Guid id)
-        {
-            var post =  _context.Post.Find(id);
-            _context.Post.Remove(post);
-             _context.SaveChanges();
-            return GetMyPosts(id);
-        }
+        
     }
 }
 
 
-/*public async Task<IActionResult> Delete(Guid id)
-        {
+      
 
-            var post = await _context.Post.FirstOrDefaultAsync(x => x.Poster == id);
-            post = ReversePosts(post);
-            return post;
-            return View("MyPosts");
-        }*/
-/*public Post[] DeletePost(Guid postid)
-{
-    Post post = _context.Post.Find(postid);
-    _context.Post.Remove(post);
-    _context.SaveChanges();
-    return (GetAllPosts());
-}
-[HttpDelete]
-public ActionResult Delete(Guid postid)
-{  
-    if (postid == null)
-    { 
-        return NotFound();
-    }  
-    var deletedpost = _postController.DeletePost(postid);
-     
-    ViewBag.Message = deletedpost;
-    //ViewBag.Title = "";
-    return View("MyPosts");
-}
-/*public ActionResult Edit(Guid id)
-{
-    var editedpost = _postController.EditPost(id);
-
-    return View("EditPost");
-}*/
-       /*[HttpPost,ActionName("Edit")]
-       public ActionResult Edit()
-       {
-       }*/
-
-       /*public Post[] EditPost(Guid postid)
-       {
-           var post = _context.Post.Where(x => x.ID == postid).FirstOrDefault();
-           return ;           
-       }*/
-       /*[HttpDelete("{id:Guid}")]
-       public async Task<Post>DeletePost(Guid id)
-       {
-           try
-           {
-               var postToDelete = await _context.Post.FirstOrDefault(x => x.ID == id);
-
-               if (postToDelete == null)
-               {
-                   return NotFound($"Employee with Id = {id} not found");
-               }
-
-               return await postToDelete;
-           }
-           catch (Exception)
-           {
-               return StatusCode(StatusCodes.Status500InternalServerError,
-                   "Error deleting data");
-           }
-       }
-    }*/
-       /*public async Task<Post>Delete(Guid id)
-       {
-           var post = await _context.Post.FindAsync(id);
-           
-
-           _context.Post.Remove(post);
-           await _context.SaveChangesAsync();
-
-           return post;
-       }*/
-       /*public void Delete(Guid? id)
-       {
-           if (id == null)
-           {
-               return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-           }
-           Post post = _context.Post.Find(id);
-           if (post == null)
-           {
-               return HttpNotFound();
-           }*/
-           
-       
-
-        
-    
