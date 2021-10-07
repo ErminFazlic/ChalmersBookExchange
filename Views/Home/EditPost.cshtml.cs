92a1a82082f6bd938d2dd75380a54e56040faa83
cynkoz07 <43467830+cynkoz07@users.noreply.github.com>
2021-10-07 12:56:58 +0200

    test

---
+++ Views/Home/EditPost.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChalmersBookExchange.Views.Home
+{
+    public class EditPost : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
